3c74b8356ebdbbb6d578238a46377da840bd0d9f
Agung Kurniadi <agungkurniadi99@gmail.com>
2024-05-07 09:09:57 +0700

    feat: add search input, switcher organization and invite component

--- src/_components/layout/navigation.tsx
+++ src/_components/layout/navigation.tsx
@@ -1,14 +1,50 @@
 'use client';
 
 import { Navbar, NavbarContent, NavbarItem } from '@nextui-org/react';
-import { UserButton } from '@clerk/nextjs';
+import {
+  OrganizationSwitcher,
+  useOrganization,
+  UserButton,
+} from '@clerk/nextjs';
+
+import { SearchInput } from '../atom';
+import { InviteButton } from '../molecul';
 
 export function Navigation() {
+  const { organization } = useOrganization();
+
   return (
-    <Navbar maxWidth="full" className="bg-green-600">
+    <Navbar maxWidth="full">
       <NavbarContent className="!items-center !justify-between">
-        <NavbarItem className="flex-1 bg-yellow-600">Search</NavbarItem>
-        <NavbarItem className="leading-none">
+        <NavbarItem className="hidden lg:flex lg:flex-1">
+          <SearchInput />
+        </NavbarItem>
+        <NavbarItem className="block flex-1 lg:hidden">
+          <OrganizationSwitcher
+            hidePersonal
+            appearance={{
+              elements: {
+                rootBox: {
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  width: '100%',
+                  maxWidth: '376px',
+                },
+                organizationSwitcherTrigger: {
+                  padding: 6,
+                  width: '100%',
+                  borderRadius: 8,
+                  border: '1px solid #E5E7EB',
+                  justifyContent: 'space-between',
+                  backgroundColor: 'white',
+                },
+              },
+            }}
+          />
+        </NavbarItem>
+        <NavbarItem className="flex items-center gap-4 leading-none">
+          {organization && <InviteButton />}
           <UserButton />
         </NavbarItem>
       </NavbarContent>
