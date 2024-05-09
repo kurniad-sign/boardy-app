26e0c3b728daef05876c641bc22a84f9ab990d99
Agung Kurniadi <agungkurniadi99@gmail.com>
2024-05-09 09:08:17 +0700

    feat: add sonner component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ConvexProvider, UIProvider } from '@/_providers';
+import { Toaster } from 'sonner';
 
 import { geist } from '@/_utils/fonts';
 
@@ -13,6 +14,18 @@ export default function RootLayout({
     <html lang="en" className={geist.className}>
       <body>
         <ConvexProvider>
+          <Toaster
+            className="toaster group:"
+            toastOptions={{
+              classNames: {
+                toast:
+                  'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:shadow-lg',
+                description: 'group-[.toast]:text-foreground-500',
+                actionButton:
+                  'group-[.toast]:bg-primary group-[.toast]:text-white',
+              },
+            }}
+          />
           <UIProvider>{children}</UIProvider>
         </ConvexProvider>
       </body>
